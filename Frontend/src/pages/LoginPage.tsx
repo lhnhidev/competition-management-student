254efc4c87e29929eadb2d40ce78b88d521c0776
lhnhidev <lhnhi420@gmail.com>
2026-06-20 15:38:00 +0700

    feat: display email and password in login page

--- Frontend/src/pages/LoginPage.tsx
+++ Frontend/src/pages/LoginPage.tsx
@@ -75,6 +75,11 @@ const LoginPage: React.FC = () => {
             </Text>
           </div>
 
+          <div className="mb-5 text-center font-bold">
+            <p>Email: nhi@gmail.com</p>
+            <p>Mật khẩu: 123456</p>
+          </div>
+
           <Form
             layout="vertical"
             onFinish={onFinish}
